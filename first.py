4456e95fd878e339b14e8e06a6e29e4e3d9fea33
lescodeurs007 <111956012+lescodeurs007@users.noreply.github.com>
2022-08-25 10:11:48 +0530

    Update first.py

--- first.py
+++ first.py
@@ -20,16 +20,4 @@ def init_connection():
 
 conn = init_connection()
 
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT * from mytable;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+st.write("Connected to:", conn.get_server_info())
